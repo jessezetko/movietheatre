451df588c79086e84587ece09c9a6d7ecc01d8c2
Jaya Jensen <jnjens47@thomasmore.edu>
2022-11-09 18:22:55 -0500

    Added images

--- Models/Product.cs
+++ Models/Product.cs
@@ -10,6 +10,8 @@ namespace movietheatre.Models
     {
         public int ID { get; set; }
 
+        public string image { get; set; }
+
         public string name { get; set; }
         
         public string description { get; set; }
