26c84c95955d68bf70df5af353e9c53ea4b67f43
Jesse <jessezetko@outlook.com>
2022-11-27 17:40:17 -0500

    admin

--- Pages/admin/Index.cshtml.cs
+++ Pages/admin/Index.cshtml.cs
@@ -6,15 +6,29 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Identity;
+using movietheatre.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace movietheatre.Pages.admin
 {
     [Authorize(Roles="admin")]
       public class IndexModel : PageModel
       {
-        public void OnGet()
+        private readonly movietheatre.Data.ApplicationDbContext _context;
+
+        public IndexModel(movietheatre.Data.ApplicationDbContext context)
         {
-            
+            _context = context;
+        }
+
+        public IList<OrderHeader> OrderHeader { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            OrderHeader = await _context.OrderHeader
+                .Include(o => o.customer)
+                .Include(o => o.orderDetails).ToListAsync();
+
         }
     }
 }
